f8e0ce26adc07c267ae3d348a79a71e5eeb72b88
ConstDet <constx7@gmail.com>
2023-05-28 13:02:07 +0300

    Отказался от toString(), воспользовался Gson

--- src/main/java/SearchEngineServer.java
+++ src/main/java/SearchEngineServer.java
@@ -1,6 +1,10 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class SearchEngineServer {
     private final int port;
@@ -18,9 +22,14 @@ public class SearchEngineServer {
                 try (Socket socket = serverSocket.accept();
                      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    out.println(engine.search(in.readLine()));
+                    out.println(toJson(engine.search(in.readLine())));
                 }
             }
         }
+    }
+
+    private String toJson(List<PageEntry> pageEntryList) {
+        Gson gson = new GsonBuilder().create();
+        return gson.toJson(pageEntryList);
     }
 }
